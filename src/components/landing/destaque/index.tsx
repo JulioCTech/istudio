ed51c711abc069090fcee3a6b83d66f9f770e319
JulioCTech <julinhubbs@gmail.com>
2025-07-30 02:24:14 -0300

    feat: implementação componente slogan

--- src/components/landing/destaque/index.tsx
+++ src/components/landing/destaque/index.tsx
@@ -1,9 +1,13 @@
 import Area from "../comum/Area";
+import Slogan from "./Slogan";
 
 export default function Destaque() {
     return (
-        <Area>
-            <div></div>
+        <Area id="inicio" className="pt-20">
+            <div className={`flex items-center h-[500px]`}>
+                <Slogan />
+            </div>
+
         </Area>
     )
 }
