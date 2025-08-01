e0aefb0645ff61c37f687f70e0642ad68db48152
Julio Cesar <132022717+JulioCTech@users.noreply.github.com>
2025-08-01 02:36:12 -0300

    Update RedeSociais.tsx

--- src/components/landing/rodape/RedeSociais.tsx
+++ src/components/landing/rodape/RedeSociais.tsx
@@ -5,10 +5,10 @@ import RedeSocial from "./RedeSocial"
 export default function RedesSociais() {
     return (
         <div className="flex">
-            <RedeSocial icone={<IconBrandYoutube />} url="https://www.youtube.com/@cod3r" />
-            <RedeSocial icone={<IconBrandInstagram />} url="https://www.instagram.com/cod3rcursos" />
-            <RedeSocial icone={<IconBrandFacebook />} url="https://www.facebook.com/cod3rcursos/" />
-            <RedeSocial icone={<IconBrandGithub />} url="https://github.com/cod3rcursos" />
+            <RedeSocial icone={<IconBrandYoutube />} url="" />
+            <RedeSocial icone={<IconBrandInstagram />} url="" />
+            <RedeSocial icone={<IconBrandFacebook />} url="" />
+            <RedeSocial icone={<IconBrandGithub />} url="" />
         </div>
     )
 }
